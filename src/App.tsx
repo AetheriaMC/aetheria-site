04e48c3a5c0420a8bc0cd6ee08f6526120cf20a5
Badbird-5907 <50347938+Badbird-5907@users.noreply.github.com>
2022-01-11 11:09:59 -0500

    more changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Homepage from "./components/Homepage";
+import Navbar from "./components/Navbar";
 
 export default class App extends React.Component {
     render() {
         return (
             <BrowserRouter>
                 <main className="">
+                    <Navbar/>
                     <Routes>
                         <Route path="/" element={
                             <React.Fragment>
-
+                                <Homepage/>
                             </React.Fragment>
                         }/>
                     </Routes>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+
+    return (
+        <>
+            <nav>
+                {
+                    /*
+                    TODO: do navbar
+                     */
+                }
+            </nav>
+        </>
+    )
+}
+export default Navbar;
